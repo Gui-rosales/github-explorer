b46ff261c1a456da0796c62d47526a11f82b7ebf
Guilherme Rosales <guilhermelexandre456@gmail.com>
2022-10-24 11:47:39 -0400

    Updated application to support Typescript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -1,9 +1,15 @@
 import { RepositoryItem } from "./RepositoryItem";
-import "../styles/repositories.scss";
 import { useState, useEffect } from "react";
+import "../styles/repositories.scss";
+
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
 
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch("https://api.github.com/users/gui-rosales/repos")
@@ -16,8 +22,10 @@ export function RepositoryList() {
       <h1>Lista de repositórios</h1>
 
       <ul>
-        {repositories.map(repository => {
-          return <RepositoryItem key={repository.name} repository={repository} />
+        {repositories.map((repository) => {
+          return (
+            <RepositoryItem key={repository.name} repository={repository} />
+          );
         })}
       </ul>
     </section>
